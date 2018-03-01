e9edc59a20c18887eaa68026e6b0e73862f3f9a2
Hans Doller <kryo2k@gmail.com>
2018-02-28 18:17:25 -0600

    Added a nodejs example of usage.

---
+++ example/person.js
@@ -0,0 +1,41 @@
+const
+_bm         = require('js-binary-model'),
+BinaryModel = _bm.BinaryModel,
+DataType    = _bm.DataType;
+
+const
+util = require('util');
+
+let Person = new BinaryModel({
+  name: {
+    type: DataType.STRING,
+    required: true
+  },
+  emailAddress: {
+    type: DataType.STRING,
+    required: false
+  },
+  userType: DataType.UINT_8,
+  balance: DataType.DOUBLE,
+  createDate: DataType.DATE,
+  metaData: DataType.JSON
+});
+
+let userEncode = Person.encode({
+  name: 'My Name',
+  emailAddress: 'my.name@mydomain.com',
+  userType: 99,
+  balance: 10000.25,
+  createDate: new Date(2018,0,1,0,0,0,0),
+  // metaData: null
+  // metaData: {
+  //   something: { else: true },
+  //   anotherThing: 7283
+  // }
+});
+
+console.log('Encoded: %s', util.inspect(userEncode.toString('base64'), true, null, true));
+
+let hansDecode = Person.decode(userEncode);
+
+console.log('Decoded: %s', util.inspect(hansDecode, true, null, true));
